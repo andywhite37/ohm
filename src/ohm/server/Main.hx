7f9e579ee4e0f7e9c5cc8dbbb5ac531664b74412
Andy White <greatwhitehope@gmail.com>
2017-03-06 21:10:02 -0700

    Initial work on server

--- src/ohm/server/Main.hx
+++ src/ohm/server/Main.hx
@@ -1,7 +1,24 @@
 package ohm.server;
 
+import js.node.Http;
+
+import express.Express;
+
+import npm.socketio.Server;
+
 class Main {
   public static function main() {
-    trace('server');
+    var port = 3000;
+    var app = new Express();
+    var server = Http.createServer(cast app);
+    var io = new npm.socketio.Server(port);
+
+    server.listen(port, function() {
+      trace('server listening on $port');
+    });
+
+    app.use(Express.serveStatic(js.Node.__dirname + '/public'));
+
+    io.onconnection()
   }
 }
